beda3e64a6d962c8bd78ab370d61f1aad38b0404
Jesus Navarrete <jesusnavarrete@Jesuss-MacBook-Pro.local>
2021-11-02 02:49:05 -0700

    completed

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -14,61 +14,55 @@ router.get("/", async (req, res, next) => {
 
 router.get("/:id", md.checkAccountId, async (req, res, next) => {
   // DO YOUR MAGIC
-  res.json(req.account);
+  try {
+    const account = await Account.getById(req.params.id)
+    res.json(account)
+  }catch (err) {
+    next(err)
+  }
 });
 
-router.post(
-  "/",
-  md.checkAccountPayload,
-  md.checkAccountNameUnique,
-  (req, res, next) => {
-    // DO YOUR MAGIC
-    try {
-      res.json("post accounts");
-    } catch (err) {
-      next(err);
-    }
+router.post('/',md.checkAccountPayload, md.checkAccountNameUnique, async (req, res, next) => {
+  try {
+    const newAccount = await Account.create({
+      name: req.body.name.trim(),
+      budget: req.body.budget,
+    })
+    res.status(201).json(newAccount)
+  } catch(err) {
+    next(err)
   }
-);
+ })
 
 router.put(
   "/:id",
   md.checkAccountId,
   md.checkAccountPayload,
-  md.checkAccountNameUnique,
-  (req, res, next) => {
-    // DO YOUR MAGIC
+  async (req, res, next) => {
     try {
-      res.json("update accounts");
+       const updated = await Account.updateById(req.params.id, req.body)
+       res.json(updated);
     } catch (err) {
       next(err);
     }
   }
 );
 
-router.delete("/:id", md.checkAccountId, (req, res, next) => {
-  // DO YOUR MAGIC
+router.delete("/:id", md.checkAccountId, async (req, res, next) => {
   try {
-    res.json("delete account");
+  await Account.deleteById(req.params.id)
+  res.json(req.account)
   } catch (err) {
     next(err);
   }
 });
 
-router.use((err, req, res, next) => {
-  // eslint-disable-line
-  // DO YOUR MAGIC
-  try {
-    res.json("get account");
-  } catch (err) {
-    next(er);
-  }
+router.use((err, req, res, next) => { 
+ res.status(err.status || 500).json({
+   message: err.message,
+ })
 });
 
-router.unsubscribe((err, req, res, next) => {
-  res.status(err.status || 500).json({
-    message: err.message,
-  });
-});
+
 
 module.exports = router;
