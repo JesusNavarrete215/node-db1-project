e1ff375761990478f609ae8a80b2bb28e70d3f11
Jesus Navarrete <jesusnavarrete@Jesuss-MacBook-Pro.local>
2021-11-01 23:17:45 -0700

    routing is mapped

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,58 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   // DO YOUR MAGIC
-})
-
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  try { res.json('get accounts')
+   
+  } catch (err) {
+    next({ status: 422, message: "this is horrible" });
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
+router.get("/:id", (req, res, next) => {
   // DO YOUR MAGIC
-})
+  try { res.json('get accounts by id')
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/", (req, res, next) => {
+  // DO YOUR MAGIC
+  try { res.json('post accounts')
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/:id", (req, res, next) => {
+  // DO YOUR MAGIC
+  try { res.json('update accounts')
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:id", (req, res, next) => {
+  // DO YOUR MAGIC
+  try { res.json('delete account')
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
-})
+  try { res.json('get account')
+  } catch (err) {
+    next(er);
+  }
+});
+
+router.unsubscribe((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+  });
+});
 
 module.exports = router;
